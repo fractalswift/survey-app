045cafa90aca836b255f8024d4fcc822f5205790
fractalswift <matthewthomasmacklin@gmail.com>
2020-03-21 12:59:34 +0000

    added api for creating new survey (basic version

--- routes/api/surveys.js
+++ routes/api/surveys.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
+const Survey = require('../../models/Survey');
+
 // @route   GET api/surveys
 // @desc    Get all surveys
 // @access  Public
@@ -17,13 +19,34 @@ router.post(
       .not()
       .isEmpty()
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('Create new survey');
+    // Check survey with same name doesn't already exist
+
+    const surveyName = req.body.surveyName;
+
+    try {
+      let survey = await Survey.findOne({ surveyName });
+
+      // If survey with this name already in db return error
+      if (survey) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Survey with this name already exists' }] });
+      }
+
+      survey = new Survey({
+        surveyName
+      });
+
+      await survey.save();
+
+      res.send('Survey created!');
+    } catch (err) {}
   }
 );
 
