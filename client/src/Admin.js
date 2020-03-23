61801bedbf50711bd124a7bb71b50e06a89f5224
fractalswift <matthewthomasmacklin@gmail.com>
2020-03-23 14:50:10 +0000

    added template admin page

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -3,7 +3,12 @@ import React from 'react';
 // TODO finish admin page once fixed fetch
 
 const Admin = () => {
-  return <div>Admin page</div>;
+  return (
+    <div>
+      <h2>Admin</h2>
+      <p>All fields required...</p>
+    </div>
+  );
 };
 
 export default Admin;
